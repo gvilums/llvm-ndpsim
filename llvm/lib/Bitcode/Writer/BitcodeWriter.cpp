168c738155f39605f197bd55c058c79f345c250d
Georgijs Vilums <georgijs.vilums@gmail.com>
2022-05-12 10:57:13 +0200

    fix unhandled case in BitcodeWriter switch

--- llvm/lib/Bitcode/Writer/BitcodeWriter.cpp
+++ llvm/lib/Bitcode/Writer/BitcodeWriter.cpp
@@ -771,6 +771,8 @@ static uint64_t getAttrKindEncoding(Attribute::AttrKind Kind) {
     return bitc::ATTR_KIND_BYREF;
   case Attribute::MustProgress:
     return bitc::ATTR_KIND_MUSTPROGRESS;
+  case Attribute::NDPSimulate:
+    return bitc::ATTR_KIND_NDP_SIMULATE;
   case Attribute::EndAttrKinds:
     llvm_unreachable("Can not encode end-attribute kinds marker.");
   case Attribute::None:
